a4ea8ebf01d8927037f30eb7c8735128acbc233f
Питенин Иван <ivan.pitenin@gmail.com>
2016-11-14 18:18:59 +0300

    finish x2

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,18 @@
 var preform = document.querySelector(".form-title");
 var form = document.querySelector(".form-index");
 var hidden = document.querySelector(".form-index");
-//preform.addEventListener("click", function (event) {
-//    event.preventDefault();
-//    form.classList.add("animated-form");
-//});
+var dateon = document.querySelector("[name=arrival]");
+var dateoff = document.querySelector("[name=departure]")
 preform.addEventListener("click", function (event) {
     if (form.classList.contains("animated-form")) {
         form.classList.remove("animated-form");
     } else {
         form.classList.add("animated-form");
+    }
+});
+form.addEventListener("submit", function (event) {
+    if (!dateon.value || !dateoff.value) {
+        event.preventDefault();
+        form.classList.add("form-error");
     }
 });
