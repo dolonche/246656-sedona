f27108d1635f77782603c25017a2947ee69f0f51
Питенин Иван <ivan.pitenin@gmail.com>
2016-11-16 18:31:16 +0300

    fix

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,11 @@
 var preform = document.querySelector(".form-title");
 var form = document.querySelector(".form-index");
 var dateon = document.querySelector("[name=arrival]");
-var dateoff = document.querySelector("[name=departure]")
+var dateoff = document.querySelector("[name=departure]");
+var link = document.querySelector(".searching");
+link.addEventListener("click", function (event) {
+     event.preventDefault(); 
+});
 preform.addEventListener("click", function (event) {
     if (form.classList.contains("animated-form")) {
         form.classList.remove("animated-form");
